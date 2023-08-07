927b8ae44e29b39c1a341c1f7a0d15972ee9ba82
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2023-08-07 13:15:09 +0700

    Tambahan fitur

--- pertemuan06/inheritance/readme.php
+++ pertemuan06/inheritance/readme.php
@@ -5,3 +5,18 @@
 
 // Child class: mewarisi semua property dan method yang dimiliki parent-nya
 // akan tetapi ada syaratnya(yang visible)
+
+// diumpamakan, ada sebuah class dengan nama mobil
+// class tersebut memiliki property / data diantaranya:
+// 1. Nama
+// 2. Merk
+// 3. Warna
+// 4. kecepatanMax
+// 5. jumlahPenumpang
+
+// lalu didalam class mobil tersebut juga terdapat method / behaviour / perilaku diantaranya:
+// 1. tambahKecepatan()
+// 1. kurangiKecepatan()
+// 2. gantiTransmisi()
+// 3. belokKiri()
+// 4. belokKanan()
